ad57d868597cd922d518f7449d64d108770caf42
Nikasan <vvlobkova@gmail.com>
2018-10-09 13:14:39 +0300

    Currency, Date and Time formatter

---
+++ hackerrank.com/Java Date and Time/src/solution2/Solution.java
@@ -0,0 +1,40 @@
+package solution2;
+import java.io.*;
+import java.util.*;
+import java.time.LocalDate;
+class Result {
+    public static String findDay(int month, int day, int year) {
+
+        int m = Integer.valueOf(month);
+        int d = Integer.valueOf(day);
+        int y = Integer.valueOf(year);
+        LocalDate date = LocalDate.of(y, m, d);
+        return date.getDayOfWeek().name();
+
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
