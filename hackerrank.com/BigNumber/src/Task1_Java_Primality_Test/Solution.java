ca040cd280542fc19f8c9e41e6c3f3df470ee152
Nikasan <vvlobkova@gmail.com>
2018-10-12 09:57:57 +0300

    Java.math.BigInteger.multiply() Method Java.math.BigInteger.add() Method

--- hackerrank.com/BigNumber/src/Task1_Java_Primality_Test/Solution.java
+++ hackerrank.com/BigNumber/src/Task1_Java_Primality_Test/Solution.java
@@ -12,6 +12,7 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         BigInteger n = scanner.nextBigInteger();
         System.out.println(n.isProbablePrime(100) ? "prime" : "not prime");
         scanner.close();
--- hackerrank.com/BigNumber/src/task2_Java_BigInteger/Solution.java
+++ hackerrank.com/BigNumber/src/task2_Java_BigInteger/Solution.java
@@ -1,9 +1,18 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+        BigInteger sum =a.add(b);
+        System.out.println(sum);
+        BigInteger mult = a.multiply(b);
+        System.out.println(mult);
+        scanner.close();
     }
 }
