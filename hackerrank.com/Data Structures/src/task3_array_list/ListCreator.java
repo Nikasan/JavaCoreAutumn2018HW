a60dc0d2b70a079c4dc609e4e6f7357ad8a668ac
Nikasan <vvlobkova@gmail.com>
2018-11-12 12:40:55 +0300

    Class for Array list learning

---
+++ hackerrank.com/Data Structures/src/task3_array_list/ListCreator.java
@@ -0,0 +1,20 @@
+package task3_array_list;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListCreator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        ArrayList<Integer>  mylist  = new ArrayList<>();
+         for (int i=0; i<n; i++){
+             mylist.add(scanner.nextInt());
+         }
+        System.out.println(mylist);
+        System.out.println("Input index:");
+        System.out.println(mylist.get(scanner.nextInt()));
+
+    }
+}
