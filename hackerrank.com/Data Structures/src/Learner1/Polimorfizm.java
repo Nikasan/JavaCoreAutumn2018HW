4b47dfd812e9400f98a21860f0bdaf68ab11fb40
Nikasan <vvlobkova@gmail.com>
2018-11-27 19:52:55 +0300

    re-write polimorfizm

--- hackerrank.com/Data Structures/src/Learner1/Polimorfizm.java
+++ hackerrank.com/Data Structures/src/Learner1/Polimorfizm.java
@@ -1,26 +1,111 @@
 package Learner1;
 
-class A {
-    String a = "I am from A";
-    void print(){
-        System.out.println(a);
+abstract class Vehicle {
+    abstract String getName();
+
+    abstract int getSize();
+}
+
+class Train extends Vehicle {
+    @Override
+    String getName() {
+        return "I am train";
+    }
+
+    @Override
+    int getSize() {
+        return 1;
+    }
+
+    public String Gudok() {
+        return "tu-tu";
     }
 }
 
-class B extends A{
-    String b="I am from B";
+class Jet extends Vehicle {
 
-    void print(){
-        System.out.println(b);
+    @Override
+    String getName() {
+        return "I am Jet";
+    }
+
+    @Override
+    int getSize() {
+        return 2;
+    }
+}
+class Helicopter extends Vehicle {
+
+    @Override
+    String getName() {
+        return null;
+    }
+
+    @Override
+    int getSize() {
+        return 100;
+    }
+}
+
+class SuperTrain extends Train {
+    @Override
+    String getName() {
+        return "I am Supertrain";
+    }
+
+    @Override
+    int getSize() {
+        return 3;
+    }
+}
+
+class Garage {
+    private void parkbySize(int size) {
+        switch (size) {
+            case (1):
+                System.out.println("parked 1");
+                break;
+            case (2):
+                System.out.println("parked 2");
+                break;
+            case (3):
+                System.out.println("parked 3");
+                break;
+            default:
+                System.out.println("unknown size-do not park");
+                break;
+        }
+    }
+
+//    public void park(Train train) {
+//        this.parkbySize(train.getSize());
+//    }
+//    public void park (Jet jet){
+//        this.parkbySize(jet.getSize());
+//    }
+////    public void park (SuperTrain supertrain){
+////        this.parkbySize(supertrain.getSize());
+////    }
+//    public  void park (Helicopter helicopter){
+//        this.parkbySize(helicopter.getSize());
+//    }
+    public void park (Vehicle vehicle){
+        this.parkbySize(vehicle.getSize());
     }
 }
 
 public class Polimorfizm {
     public static void main(String[] args) {
-        A qwerty = new A();
-        qwerty.print();
+        Train starTrain = new Train();
+        Jet jet147 = new Jet();
+        SuperTrain superTrain = new SuperTrain();
+        Helicopter heli = new Helicopter();
+        Garage garage = new Garage();
+        garage.park(superTrain);
+        garage.park(starTrain);
+        garage.park(jet147);
+        garage.park(heli);
 
-        A abcdef = new B();
-        abcdef.print();
+
     }
 }
